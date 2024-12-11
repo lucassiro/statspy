cd16a62c9f96e1c6374521b471d0594b5042369c
Lucas Siqueira <siqueiraq@gmail.com>
2024-12-11 20:06:24 -0300

    feat: implement variance and std functions

--- statspy/descriptive/dispersion.py
+++ statspy/descriptive/dispersion.py
@@ -1,13 +1,34 @@
+import math
+
+import statspy as sp
+
+
+def pvariance(array: list[int | float]) -> float:
+    mean = sp.mean(array)
+    sum_of_squares = 0.0
+    for i in array:
+        sum_of_squares += (i - mean) ** 2
+    return sum_of_squares / len(array)
+
+
 def variance(array: list[int | float]) -> float:
-    raise NotImplementedError
+    mean = sp.mean(array)
+    sum_of_squares = 0.0
+    for i in array:
+        sum_of_squares += (i - mean) ** 2
+    return sum_of_squares / (len(array) - 1)
+
+
+def pstdev(array: list[int | float]) -> float:
+    return math.sqrt(pvariance(array=array))
 
 
 def stdev(array: list[int | float]) -> float:
-    raise NotImplementedError
+    return math.sqrt(variance(array=array))
 
 
 def range(array: list[int | float]) -> float:
-    raise NotImplementedError
+    return max(array) - min(array)
 
 
 def quartiles(array: list[int | float]) -> tuple[float, float, float]:
